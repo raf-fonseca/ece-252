d4bc0796556265af57de229cbbf9450ede1f6b42
raf-fonseca <rsilvano@uwaterloo.ca>
2026-09-15 15:26:56 -0400

    is_png func

--- png_util/lab_png.c
+++ png_util/lab_png.c
@@ -1,5 +1,19 @@
 #include "lab_png.h"
 
 int is_png(U8 *buf, size_t n) {
+    
+    if (buf == NULL || n < PNG_SIG_SIZE) return 0;
+
+    U8 sig[PNG_SIG_SIZE] = {
+        0x89, 0x50, 0x4E, 0x47, 0x0D, 0x0A, 0x1A, 0x0A
+    };
+
+    // Compare each char in buf against sig
+    for (int i = 0; i < PNG_SIG_SIZE; i++) {
+        if (buf[i] != sig[i]) {
+            return 0;
+        }
+    }
+    return 1; 
 
 }
---
+++ png_util/pnginfo.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "lab_png.h"
+
+// input: ./pnginfo WEEF 1.png 
+// output: WEEF_1.png: 450 x 229
+
+// is_png takes eight bytes and check whether they match the PNG image file signature
+
+int main(int argc, char *argv[]) 
+{
+    if (argc != 2) return -1;
+    // take argv[1]
+    // open that file, if can't be opened, stop 
+    // read first 8 bytes
+    FILE* f = fopen (argv[1], "rb");
+    if (f == NULL) {
+        printf("Unable to open file!%s_ is invalid_name?\n", argv[1] );
+        return -1;
+    }
+
+    U8 buffer[PNG_SIG_SIZE];
+    size_t n = fread(buffer, 1, PNG_SIG_SIZE, f);
+
+    int blah = is_png(buffer, n);
+
+    
+    
+    
+}
